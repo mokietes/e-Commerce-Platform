308729597c1482dcc61ff37517b9fd0a2d74086d
Mekonnen T Tesfazien <102988387+mokietes@users.noreply.github.com>
2025-01-16 00:06:25 -0800

    fix

--- backend/ecommerce/src/main/java/com/ecommerce/api/controller/UserController.java
+++ backend/ecommerce/src/main/java/com/ecommerce/api/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.ecommerce.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommerce.api.model.User;
@@ -24,29 +26,33 @@ public class UserController {
         this.userService = userService;
     }
 
-    @SuppressWarnings("unchecked")
-    @GetMapping("/users")
-    public List<User> getUsers() {
-        return (List<User>) userService.getUsers();
+    // @SuppressWarnings("unchecked")
+    // @GetMapping("/users")
+    // public List<User> getUsers() {
+    //     return (List<User>) userService.getUsers();
+    // }
+
+    @GetMapping("/user")
+    public User getUser(@RequestParam int id) {
+        Optional<User> user = userService.getUser(id);
+        if (user.isPresent()) {
+            return (User) user.get();
+        }
+        return null;
     }
 
-    @GetMapping("/users/{id}")
-    public User getUser(@PathVariable int id) {
-        return userService.getUser(id);
-    }
+    // @PostMapping("/users")
+    // public void addUser(@RequestBody User user) {
+    //     userService.addUser(user);
+    // }
 
-    @PostMapping("/users")
-    public void addUser(@RequestBody User user) {
-        userService.addUser(user);
-    }
+    // @PutMapping("/users/{id}")
+    // public void updateUser(@RequestBody User user, @PathVariable int id) {
+    //     userService.updateUser(id, user);
+    // }
 
-    @PutMapping("/users/{id}")
-    public void updateUser(@RequestBody User user, @PathVariable int id) {
-        userService.updateUser(id, user);
-    }
-
-    @DeleteMapping("/users/{id}")
-    public void deleteUser(@PathVariable int id) {
-        userService.deleteUser(id);
-    }
+    // @DeleteMapping("/users/{id}")
+    // public void deleteUser(@PathVariable int id) {
+    //     userService.deleteUser(id);
+    // }
 }
--- backend/ecommerce/src/main/java/com/ecommerce/service/UserService.java
+++ backend/ecommerce/src/main/java/com/ecommerce/service/UserService.java
@@ -1,8 +1,10 @@
 package com.ecommerce.service;
 
+import java.lang.StackWalker.Option;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -15,40 +17,47 @@ public class UserService {
     public UserService() {
         userList = new ArrayList<>();
         
-        User user1 = new User(id:1, name:"John", age:5, email:"ida@mail.com");
-        User user2 = new User(id: 2, name: "Jane", age: 22, email: "jane@mail.com");
-        User user3 = new User(id: 3, name: "Doe", age: 30, email: "doe@mail.com");
-        User user4 = new User(id: 4, name: "Smith", age: 35, email: "smith@mail.com");
-        User user5 = new User(id: 5, name: "Doe", age: 30, email: "lkadfa:mail.com");
+        User user1 = new User(1, "John",5, "ida@mail.com");
+        User user2 = new User(2, "Jane", 22, "jane@mail.com");
+        User user3 = new User(3, "Doe", 30, "doe@mail.com");
+        User user4 = new User(4, "Smith", 35, "smith@mail.com");
+        User user5 = new User(5, "Doe", 30, "lkadfa@mail.com");
+        User user6 = new User(6, "Smith", 35, "smith@mail.com");
 
-
-        userList.addAll(Arrays.asList(user1, user2, user3, user4));
+        userList.addAll(Arrays.asList(user1, user2, user3, user4, user5, user6));
     }
 
-    public User getUser(int id) {
-        return userList.stream().filter(user -> user.getId() == id).findFirst().orElse(null);
+    public Optional <User> getUser(int id) {
+        Optional optional = Optional.empty();
+        for (User user : userList) {
+            if (id == user.getId()) {
+                optional = Optional.of(user);
+                return optional;
+            }
+        }
+        return optional;
     }
 
-    public User getUsers () {
-        return (User) userList;
-    }
 
-    public User deleteUser(int id) {
-        userList.removeIf(user -> user.getId() == id);
-                return null;
-    }
+    // public User getUsers () {
+    //     return (User) userList;
+    // }
 
-    public User updateUser(int id, User user) {
-        userList.stream().filter(u -> u.getId() == id).forEach(u -> {
-            u.setName(user.getName());
-            u.setAge(user.getAge());
-            u.setEmail(user.getEmail());
-        });
-                return user;
-    }
+    // public User deleteUser(int id) {
+    //     userList.removeIf(user -> user.getId() == id);
+    //             return null;
+    // }
 
-    public void addUser(User user) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addUser'");
-    }
+    // public User updateUser(int id, User user) {
+    //     userList.stream().filter(u -> u.getId() == id).forEach(u -> {
+    //         u.setName(user.getName());
+    //         u.setAge(user.getAge());
+    //         u.setEmail(user.getEmail());
+    //     });
+    //             return user;
+    // }
+
+    // public void addUser(User user) {
+    //     userList.add(user); 
+    // }
 }
