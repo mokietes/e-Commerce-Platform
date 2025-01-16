ad2935cfd4f357f584dc9baa343ac9a4688c8aa9
Mekonnen T Tesfazien <102988387+mokietes@users.noreply.github.com>
2025-01-15 23:34:41 -0800

    fix

--- backend/ecommerce/src/main/java/com/ecommerce/service/UserService.java
+++ backend/ecommerce/src/main/java/com/ecommerce/service/UserService.java
@@ -14,10 +14,13 @@ public class UserService {
 
     public UserService() {
         userList = new ArrayList<>();
-        User user1 = new User(id: 1, name: "John", age: 25, email: "ida@mail.com");
+        
+        User user1 = new User(id:1, name:"John", age:5, email:"ida@mail.com");
         User user2 = new User(id: 2, name: "Jane", age: 22, email: "jane@mail.com");
         User user3 = new User(id: 3, name: "Doe", age: 30, email: "doe@mail.com");
         User user4 = new User(id: 4, name: "Smith", age: 35, email: "smith@mail.com");
+        User user5 = new User(id: 5, name: "Doe", age: 30, email: "lkadfa:mail.com");
+
 
         userList.addAll(Arrays.asList(user1, user2, user3, user4));
     }
@@ -42,5 +45,10 @@ public class UserService {
             u.setEmail(user.getEmail());
         });
                 return user;
+    }
+
+    public void addUser(User user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addUser'");
     }
 }
