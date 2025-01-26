061da0a4b708acb052ecaa58294ea060050af205
Mekonnen T Tesfazien <102988387+mokietes@users.noreply.github.com>
2025-01-25 23:01:11 -0500

    Update UserController.java

--- backend/ecommerce/src/main/java/com/ecommerce/api/controller/UserController.java
+++ backend/ecommerce/src/main/java/com/ecommerce/api/controller/UserController.java
@@ -22,7 +22,7 @@ import com.ecommerce.service.UserService;
 public class UserController {
     
     private final UserService userService;
-
+    
     public UserController(UserService userService) {
         this.userService = userService;
     }
